98feb8598b87e8e997a0db0e04b9605dd843e8e8
Sankgao <sankgao@163.com>
2024-02-23 18:28:13 +0800

    :memo: 修改脚本

--- c/hello-world/hello.c
+++ c/hello-world/hello.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-    printf("你好 Hello World\n");
-    // 暂停程序，方便观察程序执行结果
-    system("pause");
+    printf("Hello World\n");
+
     return 0;
 }
