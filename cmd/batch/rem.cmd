903250632e3844776bef38a9ef9609e4414e8350
Sank Gao <sankgao@163.com>
2024-07-30 17:54:57 +0800

    :memo: 添加批处理（Batch）示例脚本

---
+++ cmd/batch/rem.cmd
@@ -0,0 +1,14 @@
+@echo off
+:: "Hello, World!"
+rem "Hello, World!"
+
+echo "Hello, World! 01"  :: "Hello, World!"
+echo "Hello, World! 02"  rem "Hello, World!"
+echo "Hello, World! 03"  %行内注释%
+
+Rem/||(
+  The REM statement evaluates to success,
+  so these lines will never be executed.
+  Keep in mind that you will need to escape closing parentheses
+  within multi-line comment blocks like shown in this example. ^)
+)
