0cfbdda9e15c5ef5c8aea6d402102cd4e376a427
Sank Gao <sankgao@163.com>
2024-08-01 18:02:44 +0800

    :memo: 修改批处理示例脚本

--- cmd/batch/echo.cmd
+++ cmd/batch/echo.cmd
@@ -1,19 +1,19 @@
-rem 示例 1：批处理（Batch）默认打开回显功能
+rem 示例 1：批处理（Batch）默认打开回显功能。 
 echo
 echo "Hello, World! 01"
 
-rem 示例 2：显示空白行
+rem 示例 2：显示空白行。 
 echo.
 
-rem 示例 3：使用 @ 符号，关闭本行命令的回显
+rem 示例 3：使用 @ 符号，关闭本行命令的回显。 
 echo "Hello, World! 02"
 @echo "Hello, World! 03"
 
-rem 示例 4：使用 echo off 关闭下面命令回显功能，但 echo off 命令的回显依然显示
+rem 示例 4：使用 echo off 关闭下面命令回显功能，但 echo off 命令的回显依然显示。 
 echo off
 echo "Hello, World! 04"
 echo on
 
-rem 示例 5：使用 @ 符号，将 echo off 的命令回显也关闭
+rem 示例 5：使用 @ 符号，将 echo off 的命令回显也关闭。 
 @echo off
 echo "Hello, World! 05"
--- cmd/batch/pause.cmd
+++ cmd/batch/pause.cmd
@@ -1,12 +1,12 @@
 @echo off
-rem 示例 1：用 pause 命令使批处理程序在指定位置暂定
+rem 示例 1：用 pause 命令使批处理程序在指定位置暂定。 
 echo "Hello, World!"
 pause
 
-rem 示例 2：用 pause 命令使批处理程序在指定位置暂定，但不显示提示内容
+rem 示例 2：用 pause 命令使批处理程序在指定位置暂定，但不显示提示内容。 
 echo "Hello, World!"
-pause > null
+pause > nul
 
-rem 示例 1：用 pause 命令使批处理程序在指定位置暂定，并修改提示内容为 “按任意键继续或退出此脚本. . .”
+rem 示例 3：用 pause 命令使批处理程序在指定位置暂定，并修改提示内容为 “按任意键继续或退出此脚本. . .”。 
 echo "Hello, World!"
 echo 按任意键继续或退出此脚本. . . & pause > nul
