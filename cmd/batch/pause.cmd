22b26b437c671f2002ed44c3bd6c4d719bf0ca38
Sank Gao <sankgao@163.com>
2024-08-02 18:08:12 +0800

    :memo: 更新批处理示例脚本

--- cmd/batch/pause.cmd
+++ cmd/batch/pause.cmd
@@ -1,12 +1,12 @@
 @echo off
-rem 示例 1：用 pause 命令使批处理程序在指定位置暂定。 
-echo "Hello, World!"
+rem 示例 1：用 pause 命令使批处理程序在指定位置暂停。
+echo Hello, World!
 pause
 
-rem 示例 2：用 pause 命令使批处理程序在指定位置暂定，但不显示提示内容。 
-echo "Hello, World!"
+rem 示例 2：用 pause 命令使批处理程序在指定位置暂停，但不显示提示内容。
+echo Hello, World!
 pause > nul
 
-rem 示例 3：用 pause 命令使批处理程序在指定位置暂定，并修改提示内容为 “按任意键继续或退出此脚本. . .”。 
-echo "Hello, World!"
+rem 示例 3：用 pause 命令使批处理程序在指定位置暂停，并修改提示内容为 “按任意键继续或退出此脚本. . .”。
+echo Hello, World!
 echo 按任意键继续或退出此脚本. . . & pause > nul
--- cmd/batch/start.cmd
+++ cmd/batch/start.cmd
@@ -1,9 +1,9 @@
 @echo off
-echo "在新 CMD 控制台窗口中查看 D:\ 盘目录"
+echo 在新【命令提示符】窗口中查看 D:\ 盘根目录信息。
 start dir D:\
 
-echo "运行计算器程序，等程序启动后再执行下面的命令. . ."
+echo 运行计算器程序，等程序启动后再执行下面的命令. . .
 start /wait calc.exe
+echo 程序成功启动，执行下面的命令。
 
-echo "程序成功启动，执行下面的命令"
-echo "Hello, World!"
+echo Hello, World!
