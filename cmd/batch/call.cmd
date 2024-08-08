f85103bea0b0440b12f26e50d48731966d4b8133
Sank Gao <sankgao@163.com>
2024-08-08 18:17:46 +0800

    :memo: 更新示例脚本

--- cmd/batch/call.cmd
+++ cmd/batch/call.cmd
@@ -1,7 +1,16 @@
 @echo off
-rem 示例：使用 call 命令执行脚本，并添加两个参数。 
+rem 示例 1：使用 call 命令执行脚本，并添加两个参数。 
 echo 脚本名为：%0
 echo 第一个参数为：%1
 echo 第二个参数为：%2
 
 echo 脚本名为：%0，第一个参数为：%~1，第二个参数为：%2
+
+rem 示例 2：使用 call 命令跳转到指定标签处执行，标签处执行后再回到 call 命令后继续执行。 
+set var=test
+call :deal %var% demo
+:deal
+echo %%0 = %0
+echo %%1 = %1
+echo %%2 = %2
+pause
--- cmd/batch/for.cmd
+++ cmd/batch/for.cmd
@@ -9,11 +9,30 @@ for %%num in (666, 520, 233) do (
     echo %%num
 )
 
-rem 示例 3：/l 将通过比较 start# 和 end# 来执行迭代。如果 start# 小于 end#，则执行该命令。 
-rem 当迭代变量超过 end# 时，命令 shell 退出循环。 
-rem 还可以使用 step# 来逐步减少值的范围。 
-rem 例如：(1,1,5) 生成序列 1 2 3 4 5；(5,-1,1) 生成序列 5 4 3 2 1。
-for /l %%a in (0,2,10) do (
+rem 示例 3：for 命令中 /d 选项，用于匹配目录，忽略文件；* 表示当前目录下的所有文件。 
+echo 文件有：
+for %%f in (*) do echo %%f
+
+echo 目录有：
+for /d %%d in (*) do echo %%d
+
+rem 示例 4：for 命令中 /r 选项，用于指定工作目录；当与 /d 选项一起使用时，/d 必须写在 /r 选项前面。 
+echo 文件有：
+for /r E:\test %%f in (*) do echo %%f
+
+echo 目录有：
+for /d /r E:\test %%d in (*) do echo %%d
+
+rem 示例 5：for 命令中 /l 选项，用于指定每次循环的步数。 
+rem 语法：for /l %%variable in (stat, step, end) do (cmmand)；start 表示开始循环，end 表示结束循环，step 表示每次循环的步数。 
+rem 例如：显示 10 以内的偶数。
+for /l %%a in (2, 2, 10) do (
   echo %%a
 )
-pause
+
+rem 示例 6：for 命令中 /f 选项，用于指定高级扩展。 
+rem 例如：将 “DIR” 命令的输出信息作为遍历内容，首先跳过前两行，然后从第三个字符块开始取值，使用默认的方式进行换行或截断操作， 
+rem 其中第二个字符块将作为 “%%a” 的值，第三个字节块作为 “%%b” 的值，第三个之后的字节块作为 “%%c” 的值。
+for /f "skip=2 tokens=2* delims= " %%a in ('dir') do (
+  echo %%a--%%b--%%c
+)
